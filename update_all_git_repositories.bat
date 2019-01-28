c006bf6f2899e2eb08ef4a228d20714d6672bc06
Vincent Wetzel <31972246+vincentwetzel@users.noreply.github.com>
2019-01-28 00:53:01 -0700

    Create update_all_git_repositories.bat

---
+++ update_all_git_repositories.bat
@@ -0,0 +1,8 @@
+@echo off
+
+git -C E:\coding_workspaces\Python\YouTube_Downloader\ pull https://github.com/vincentwetzel/YouTube_Downloader
+git -C E:\coding_workspaces\Python\IG_Sorter\ pull https://github.com/vincentwetzel/IG_Sorter
+git -C E:\coding_workspaces\Python\Podcast_Splitter\ pull https://github.com/vincentwetzel/Podcast_Splitter
+
+::pause
+exit
